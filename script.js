b1f7e330433bdc6ec0a34e424ced38ad19bafd7e
Erick Jensen <Erick.Jensen010@gmail.com>
2021-05-06 08:59:20 -0500

    add more styling, continue adding javascript logic

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 console.log('Hello World');
 
-const bill = document.querySelector('.bill');
+const total = document.querySelector('.total');
+
+const calculate = () => {
+    const html = `<h3>Total</h3>`;
+    total.innerHTML = html;
+}
+
+
+
+const bill = document.querySelector('.bill').value;
 console.log(bill);
 
-const tipPercentage = document.querySelector('.tip_percentage');
+const tipPercentage = document.querySelector('.tip_percentage').value;
 console.log(tipPercentage);
+
+calculate();
 
