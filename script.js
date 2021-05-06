aac0733d30159c3f650d37f43a53bfc6f63939ef
Erick Jensen <Erick.Jensen010@gmail.com>
2021-05-05 21:22:37 -0500

    fix formating, start working on the javascript

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('Hello World');
+
+const bill = document.querySelector('.bill');
+console.log(bill);
+
+const tipPercentage = document.querySelector('.tip_percentage');
+console.log(tipPercentage);
+
