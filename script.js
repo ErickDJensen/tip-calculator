9c26f4a22c6870513fbd0f0e6927c9df8ed8e905
Erick Jensen <Erick.Jensen010@gmail.com>
2021-05-07 15:33:58 -0500

    finish javascript login

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 console.log('Hello World');
 
 const total = document.querySelector('.total');
+const button = document.querySelector('.button')
 
 const calculate = () => {
-    const html = `<h3>Total</h3>`;
+    console.log('in calculate');
+    const bill = document.querySelector('.bill').value;
+    console.log(bill);
+    const tipPercentage = document.querySelector('.tip_percentage').value;
+    console.log(tipPercentage);
+    const html = `<h3>Total</h3>
+    $${((bill * tipPercentage) + Number(bill)).toFixed(2)}`;
     total.innerHTML = html;
 }
 
 
 
-const bill = document.querySelector('.bill').value;
-console.log(bill);
+button.addEventListener("click", calculate);
 
-const tipPercentage = document.querySelector('.tip_percentage').value;
-console.log(tipPercentage);
 
-calculate();
 
